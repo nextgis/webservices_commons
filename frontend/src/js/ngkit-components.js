2a3d6319d984b6ace95984fc401b61e6d7b51006
lisovenkoNastya <lisovenko.ad@gmail.com>
2020-06-29 14:30:26 +0400

    feat: add NGUI kit components

--- frontend/src/js/ngkit-components.js
+++ frontend/src/js/ngkit-components.js
@@ -3,3 +3,11 @@ import Vue from 'vue';
 Vue.component(
   'NgButton', () => import('../components/NgButton/NgButton')
 );
+
+Vue.component(
+  'NgSheet', () => import('../components/NgSheet/NgSheet')
+);
+
+Vue.component(
+  'NgPrice', () => import('../components/NgPrice/NgPrice')
+);
