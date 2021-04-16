ebb0336d485d07311c67d3b42a26d2d59f12c27b
Sergey Kazarin <mechmskv@yandex.ru>
2021-04-16 12:43:31 +0300

    disabling AccessToken in settings if needed

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from __future__ import unicode_literals
 
-from nextgis_common.ngid_auth.models import AccessToken
+from django.conf import settings
+
+without_accesstoken = False
+if hasattr(settings, 'IS_DISABLED_COMMON_ACCESSTOKEN'):
+    if settings.IS_DISABLED_COMMON_ACCESSTOKEN:
+        without_accesstoken = True
+
+if not without_accesstoken:
+    from nextgis_common.ngid_auth.models import AccessToken
