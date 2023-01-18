e8c256fa763639d40abb47031669147efe3d9a5e
mechmskv <mechmskv@gmail.com>
2023-01-18 17:28:09 +0300

    csrf_exempt on /logout

--- ngid_auth/urls.py
+++ ngid_auth/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from nextgis_common.ngid_auth.views import NgidOAuth2CallbackView, NgidOAuth2LoginView, NgidLogoutView
-
+from django.views.decorators.csrf import csrf_exempt
 
 prefix = getattr(settings, 'OAUTH_URLS_PREFIX', '')
 
@@ -13,6 +13,6 @@ def get_url_pattern(base_url_pattern):
 urlpatterns = [
     url(get_url_pattern('login'), NgidOAuth2LoginView.as_view(), name=NgidOAuth2LoginView.view_name),
     url(get_url_pattern('login/callback'), NgidOAuth2CallbackView.as_view(), name=NgidOAuth2CallbackView.view_name),
-    url(get_url_pattern('logout'), NgidLogoutView.as_view(), name=NgidLogoutView.view_name)
+    url(get_url_pattern('logout'), csrf_exempt(NgidLogoutView.as_view()), name=NgidLogoutView.view_name)
 ]
 
