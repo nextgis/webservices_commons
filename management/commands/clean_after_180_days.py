f8eaaba20a89e126947bee021214b05d3ab6edc0
mechmskv <mechmskv@gmail.com>
2023-02-01 18:24:19 +0300

    handle folders

--- management/commands/clean_after_180_days.py
+++ management/commands/clean_after_180_days.py
@@ -1,3 +1,5 @@
+import shutil
+
 from django.core.management.base import BaseCommand
 from django.conf import settings
 import os
@@ -21,6 +23,8 @@ class Command(BaseCommand):
             try:
                 # folder = settings.SENDFILE_ROOT
                 print(f'clean_after_180_days: examining folder {folder}')
+                if do_delete:
+                    print('WITH DELETE OPTION!')
 
                 now = time.time()
                 i = 0
@@ -31,26 +35,44 @@ class Command(BaseCommand):
                 files.sort(key=os.path.getmtime)
                 fsize_total = 0
                 for f in files:
+                    is_dir = os.path.isdir(f)
                     file = os.stat(f)
                     ago_180 = now - 180 * 86400
                     # ago_180 = now - 18
                     if file.st_mtime < ago_180:
                         i += 1
-                        fsize = os.path.getsize(f)
-                        fsize_total += fsize
-                        fsize = self._get_fsize_mb(fsize)
                         fcreated = datetime.fromtimestamp(file.st_mtime)
                         fcreated = fcreated.strftime('%Y-%m-%d %H:%M')
                         basename = os.path.basename(f)
-                        if do_delete == 1:
-                            os.remove(f)
-                        print(f'[{i}] {fcreated}   ({fsize} mb) {basename}')
+                        if is_dir:
+                            fsize = self._get_folder_size(f)
+                            if do_delete == 1:
+                                shutil.rmtree(f)
+                        else:
+                            fsize = os.path.getsize(f)
+                            if do_delete == 1:
+                                os.remove(f)
+                        fsize_mb = self._get_fsize_mb(fsize)
+                        fsize_total += fsize
+                        print(f'[{i}] {fcreated}   ({fsize_mb} mb) {basename}')
                 fsize_total = self._get_fsize_mb(fsize_total)
                 print(f'====================================\r\nTOTAL FSIZE: {fsize_total} mb')
             except Exception as e:
                 print(e)
             finally:
                 time.sleep(3600)
+
+    @classmethod
+    def _get_folder_size(cls, start_path='.'):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(start_path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                # skip if it is symbolic link
+                if not os.path.islink(fp):
+                    total_size += os.path.getsize(fp)
+
+        return total_size
 
     def _get_fsize_mb(self, fsize):
         fsize = fsize / (1024 * 1024)
