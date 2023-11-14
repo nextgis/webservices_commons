c5f72fa933b36b4865f7097c2aaa6d6b380424b3
mechmskv <mechmskv@gmail.com>
2023-11-14 10:12:51 +0300

    more logging

--- ngid_auth/mixins.py
+++ ngid_auth/mixins.py
@@ -3,27 +3,40 @@ from __future__ import unicode_literals
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 import os
+import logging
+
+logger = logging.getLogger('nextgis_common.ngid_auth.backands')
 
 
 class OAuthClientMixin(object):
 
     @classmethod
     def do_get_oauth_session(cls, provider, token=None, scope=None):
+        cl_id = provider.consumer_key
+        cl_se = provider.consumer_secret
         extra = {
-            'client_id': provider.consumer_key,
-            'client_secret': provider.consumer_secret,
+            'client_id': cl_id,
+            'client_secret': cl_se,
         }
         verify = os.getenv('SSL_VERIFY', False)
         if not verify:
             verify = False
-
+        scp = scope or provider.scopes
+        r_url = provider.access_token_url
         oaut_session = OAuth2Session(
-            client_id=provider.consumer_key,
-            scope=scope or provider.scopes,
+            client_id=cl_id,
+            scope=scp,
             token=token,
-            auto_refresh_url=provider.access_token_url,
+            auto_refresh_url=r_url,
             auto_refresh_kwargs=extra,
         )
+        logger.info(f'do_get_oauth_session(): '
+                    f'client_id: {cl_id}, '
+                    f'scope: {scp}, '
+                    f'auto_refresh_url: {r_url}, '
+                    f'auto_refresh_kwargs: {extra}, '
+                    f'token: {token}'
+                    )
         oaut_session.verify = verify
         oaut_session.redirect_uri = None
         return oaut_session
