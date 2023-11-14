6d61999f443ddd4869404f9555dec8ac725f7b78
mechmskv <mechmskv@gmail.com>
2023-11-14 11:04:59 +0300

    multiple fixes

--- admin.py
+++ admin.py
@@ -7,10 +7,16 @@ class OAuthStateAdmin(admin.ModelAdmin):
     list_display = ('value', 'client_id', 'created')
     search_fields = ('value', 'client_id')
 
+    def get_ordering(self, request):
+        return ['-created']
+
 
 class AccessTokenAdmin(admin.ModelAdmin):
     list_display = ('user', 'created', 'modified', 'expires_at', 'state', 'access_token', 'refresh_token', 'access_token_id')
     search_fields = ('access_token', 'refresh_token')
+
+    def get_ordering(self, request):
+        return ['-expires_at']
 
 
 admin.site.register(OAuthState, OAuthStateAdmin)
