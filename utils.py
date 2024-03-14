68cbc0d720960ef32faa80d899da4d8645582fcb
mechmskv <mechmskv@gmail.com>
2024-03-14 14:26:14 +0300

    unsafe redirect fix ( https://app.clickup.com/t/86942e3z6 )

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from django.utils import translation
+from django.conf import settings
+from urllib.parse import urlparse
 
 
 def activate_user_locale(request, user_locale):
@@ -6,3 +8,12 @@ def activate_user_locale(request, user_locale):
     request.session[translation.LANGUAGE_SESSION_KEY] = user_locale
     #response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_locale)
 
+
+def sanitize_url(url):
+    allowed_hosts = settings.ALLOWED_HOSTS
+    parsed = urlparse(url)
+    if parsed.netloc:
+        if parsed.netloc not in allowed_hosts:
+            print('suspicious domain detected')
+            return '/'
+    return url
